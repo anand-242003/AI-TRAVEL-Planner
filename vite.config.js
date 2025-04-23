fff4d25095a688b19c4fad16bd3db2824137f054
zenithglint <anandmishra1@Anands-MacBook-Pro.local>
2025-04-23 11:09:57 +0530

    g

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AI-TRAVEL-Planner/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
